f88d7e537a59705e1045efb4b52bd09dcbefe000
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-04-26 15:54:06 -0700

    add post route

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json');
 const fs = require('fs');
+const {readAndAppend, readFromFile} = require('./utils');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -21,37 +21,25 @@ app.get('/notes', (req, res) => {
    res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-app.get('/api/notes', (req, res) => res.json(notes));
+app.get('/api/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 app.post('/api/notes', (req, res) => {
     const { title, text} = req.body;
-
+    console.log(req.body);
     // If all the required properties are present
     if (title && text){
-       
         const newNote = {
             title,
             text
         };
-
-        // Obtain existing reviews
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-            } else {
-
-                const parsedNotes = JSON.parse(data);
-                parsedNotes.push(newNote);
-                fs.writeFile(
-                './db/db.json',
-                JSON.stringify(parsedNotes, null, 4),
-                (writeErr) =>
-                    writeErr
-                    ? console.error(writeErr)
-                    : console.info('Successfully updated Notes!')
-                );
-            }
-        });
+        readAndAppend(newNote, './db/db.json');
+        
+        res.json('Data appended successfully');   
+    }
+    else{
+        res.json('Error in posting a note');
     }
 });
 
